fc02acfbae6a0ddb58a2941006e120da71585cd5
kafku <kazunoko93@gmail.com>
2019-02-25 21:34:15 +0900

    add script to download images

---
+++ data/download_images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eu
+
+# download images form flickr (this takes some time)
+echo prepareing folders...
+for i in $(seq 10 99); do
+	mkdir -p ./images/${i}
+done
+
+echo downloading images...
+cut -d' ' -f2 ./image_urls.txt | tail -n 10 | xargs -L 1 -P 1 -I{} \
+	bash -c '[ ! -f ./images/$(basename {} | cut -c1-2)/$(basename {}) ] && wget --wait=2.5 --max-redirect 0 --random-wait -P ./images/$(basename {} | cut -c1-2) {}'
+
+#find ./images/ -name "*jpg" | xargs -I"{}" bash -c 'convert -colorspace RGB {} `echo {} | sed -e "s/images/color_images/g"`'
