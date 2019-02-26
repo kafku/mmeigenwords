900617c99115a725bd20bb869ebb6478e949a086
kafku <kazunoko93@gmail.com>
2019-02-27 00:38:29 +0900

    removed unnecessary operation

--- data/download_images.sh
+++ data/download_images.sh
@@ -8,7 +8,7 @@ for i in $(seq 10 99); do
 done
 
 echo downloading images...
-cut -d' ' -f2 ./image_urls.txt | tail -n 10 | xargs -L 1 -P 1 -I{} \
+cut -d' ' -f2 ./image_urls.txt | xargs -L 1 -P 1 -I{} \
 	bash -c '[ ! -f ./images/$(basename {} | cut -c1-2)/$(basename {}) ] && wget --wait=2.5 --max-redirect 0 --random-wait -P ./images/$(basename {} | cut -c1-2) {}'
 
 #find ./images/ -name "*jpg" | xargs -I"{}" bash -c 'convert -colorspace RGB {} `echo {} | sed -e "s/images/color_images/g"`'
