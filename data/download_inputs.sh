698c1718f6e0b9e65ae0b19dc98ea62f651d2d20
kafku <kazunoko93@gmail.com>
2019-02-26 16:49:35 +0900

    download benchmark datasets

--- data/download_inputs.sh
+++ data/download_inputs.sh
@@ -6,3 +6,13 @@ set -eu
 wget --no-check-certificate -O ./inputs.7z "https://onedrive.live.com/download?cid=AC7356DF64222A7C&resid=AC7356DF64222A7C%2111749&authkey=AAHuOht2wFTuE5o"
 7z x ./inputs.7z
 rm ./inputs.7z
+
+
+# download benchmark datasets
+## wordsim353
+wget http://www.cs.technion.ac.il/~gabr/resources/data/wordsim353/wordsim353.zip
+unzip wordsim353.zip
+rm wordsim353.zip
+
+## analogy task
+wget https://raw.githubusercontent.com/tmikolov/word2vec/master/questions-words.txt
